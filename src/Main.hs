fe0c36d5d37c790af58d812babc9fe96c791a773
Deltaqxq <deltaqxq174@mail.ru>
2026-08-11 14:47:22 +0500

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,41 +1,42 @@
 module Main where
 
--- Import my modules 
+-- Import my modules
 import Terminals.Alacritty
-import Terminals.Kitty 
+import Terminals.Kitty
 import Terminals.Ghostty
+import Terminals.Unknown
 
--- Import not my modules 
+-- Import not my modules
 import System.Environment (lookupEnv)
 import Data.List (isInfixOf)
 
 data Terminal = Kitty | Alacritty | Ghostty | Unknown
     deriving (Show, Eq)
 
-detectTerminal :: IO Terminal 
+detectTerminal :: IO Terminal
 detectTerminal = do
     maybeTerm <- lookupEnv "TERM"
     maybeKittyPid <- lookupEnv "KITTY_PID"
 
-    return $ case maybeTerm of 
-        Just t | "kitty" `isInfixOf` t -> Kitty 
-        Just t | "alacritty" `isInfixOf` t -> Alacritty 
+    return $ case maybeTerm of
+        Just t | "kitty" `isInfixOf` t -> Kitty
+        Just t | "alacritty" `isInfixOf` t -> Alacritty
         Just t | "ghostty" `isInfixOf` t -> Ghostty
-        _ -> case maybeKittyPid of 
-            Just _ -> Kitty 
+        _ -> case maybeKittyPid of
+            Just _ -> Kitty
             Nothing -> Unknown
 
 runKitty :: IO ()
-runKitty = do kitty 
+runKitty = do kitty
 
 runAlacritty :: IO ()
 runAlacritty = do alacritty
 
 runGhostty :: IO ()
-runGhostty = do ghostty 
+runGhostty = do ghostty
 
 runGeneric :: IO ()
-runGeneric = do putStrLn "sdfsdfsdfswerwefsdghfghfa"
+runGeneric = do unknown
 
 executeFeature :: Terminal -> IO ()
 executeFeature Kitty     = runKitty
@@ -46,4 +47,4 @@ executeFeature Unknown   = runGeneric
 main :: IO ()
 main = do
     term <- detectTerminal
-    executeFeature term 
+    executeFeature term
