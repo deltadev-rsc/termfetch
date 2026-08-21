9eacb7d6d811b94a2342bdc70b8ddc336bbc4f54
Deltaqxq <deltaqxq174@mail.ru>
2026-08-21 21:02:23 +0500

    Update Alacritty.hs

--- src/Terminals/Alacritty.hs
+++ src/Terminals/Alacritty.hs
@@ -1,22 +1,6 @@
 module Terminals.Alacritty where
 
-reset :: String
-red :: String
-green :: String
-yellow :: String
-blue :: String
-magenta :: String
-cyan :: String
-orange :: String
-
-reset = "\x1b[0m"
-red = "\x1b[31m"
-green = "\x1b[32m"
-yellow = "\x1b[33m"
-blue = "\x1b[34m"
-magenta = "\x1b[35m"
-cyan = "\x1b[36m"
-orange = "\x1b[38;5;208m"
+import Terminals.Lib.Colors 
 
 alacritty :: IO ()
 alacritty = do
