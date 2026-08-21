aaa1df4263c639224434672fb613365caf729902
Deltaqxq <deltaqxq174@mail.ru>
2026-08-21 21:02:01 +0500

    Update Kitty.hs

--- src/Terminals/Kitty.hs
+++ src/Terminals/Kitty.hs
@@ -1,24 +1,6 @@
 module Terminals.Kitty where
 
-reset :: String
-red :: String
-green :: String
-yellow :: String
-blue :: String
-magenta :: String
-cyan :: String
-orange :: String
-
-reset = "\x1b[0m"
-black = "\x1b[30m"
-red = "\x1b[31m"
-green = "\x1b[32m"
-yellow = "\x1b[33m"
-blue = "\x1b[34m"
-magenta = "\x1b[35m"
-cyan = "\x1b[36m"
-orange = "\x1b[38;5;208m"
-white = "\x1b[79m"
+import Terminals.Lib.Colors 
 
 kitty :: IO ()
 kitty = do
